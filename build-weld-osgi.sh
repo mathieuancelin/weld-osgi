d7a77ef4899c217a848d7f31262f14c37299d7f0
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-01-22 19:53:26 +0100

    Fix in the script

--- build-weld-osgi.sh
+++ build-weld-osgi.sh
@@ -3,6 +3,6 @@
 
 clear;
 #mvn clean install;
-cd ./container/target/osgi-cdi-container-1.0-SNAPSHOT-all/osgi-cdi-container-1.0-SNAPSHOT/;
+cd ./container/target/osgi-cdi-container-1.0-SNAPSHOT-all.dir/osgi-cdi-container-1.0-SNAPSHOT/;
 java -jar bin/felix.jar;
 
