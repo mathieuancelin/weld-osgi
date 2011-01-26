32515718d96ba426252fe7ff42ceeffd60b908f7
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-01-26 18:45:28 +0100

    Add event listening

--- sample/bundle-gui/src/main/java/com/sample/osgi/cdi/startable/internal/ServiceImpl.java
+++ sample/bundle-gui/src/main/java/com/sample/osgi/cdi/startable/internal/ServiceImpl.java
@@ -1,10 +1,14 @@
 package com.sample.osgi.cdi.startable.internal;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import javax.enterprise.event.Observes;
+import org.jboss.weld.environment.osgi.api.extension.Publish;
 import org.jboss.weld.environment.osgi.api.extension.Startable;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.ServiceEvent;
 
 @Startable
+@Publish
 public class ServiceImpl {
 
     @PostConstruct
@@ -12,7 +16,15 @@ public class ServiceImpl {
         System.out.println("init");
     }
 
-    @PreDestroy
+    public void listen(@Observes ServiceEvent event) {
+        System.out.println("listen " + event);
+    }
+
+    public void listen2(@Observes BundleEvent event) {
+        System.out.println("listen " + event);
+    }
+
+    @javax.annotation.PreDestroy
     public void stop() {
         System.out.println("stop");
     }
