22f50b7c71c8429145ee96fad50555c5da931379
Guillaume Sauthier <guillaume.sauthier@gmail.com>
2011-01-28 00:11:39 +0100

    Continue separation of integration/separation
    * extension is now only responsible to send OSGi events
    * integration is reponsible to start/stop management of Bundles

--- weld-osgi-impl/src/main/java/org/jboss/weld/environment/osgi/extension/CDIActivator.java
+++ weld-osgi-impl/src/main/java/org/jboss/weld/environment/osgi/extension/CDIActivator.java
@@ -1,108 +1,63 @@
 package org.jboss.weld.environment.osgi.extension;
 
-import java.util.concurrent.ConcurrentHashMap;
-import org.jboss.weld.bootstrap.api.SingletonProvider;
-import org.jboss.weld.bootstrap.api.helpers.IsolatedStaticSingletonProvider;
-import org.jboss.weld.environment.osgi.api.integration.CDIOSGiContainer;
-import org.jboss.weld.environment.osgi.api.integration.CDIOSGiContainerFactory;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleEvent;
-import org.osgi.framework.BundleListener;
-import org.osgi.framework.ServiceEvent;
-import org.osgi.framework.ServiceListener;
-import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.framework.*;
+
+import javax.enterprise.event.Event;
 
 /**
- * Entry point of the OSGi Bundle. Start the Weld container and listen to bundle
- * events.
+ * It seems we cannot get the BundleContext in the Extension, so
+ * to fire up OSGi Events (BundleEvent, ServiceEvent and FrameworkEvent)
+ * we need to act here.
  *
  * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
  */
-public class CDIActivator implements BundleActivator, BundleListener
-        , ServiceListener {
-
-    private final ConcurrentHashMap<Long, CDIOSGiContainer> containers;
-    private CDIOSGiContainerFactory factory;
-
-    public CDIActivator() {
-        containers = new ConcurrentHashMap<Long, CDIOSGiContainer>();
-    }
+public class CDIActivator implements BundleActivator,
+                                     BundleListener,
+                                     ServiceListener {
 
     @Override
     public void start(BundleContext context) throws Exception {
-        SingletonProvider.initialize(new IsolatedStaticSingletonProvider());
-        ServiceTracker tracker = new ServiceTracker(context, CDIOSGiContainerFactory.class.getName(), null);
-        tracker.open();
-
-        // TODO not very dynamic ;)
-        factory = (CDIOSGiContainerFactory) tracker.waitForService(10000);
-        //ServiceReference ref = context.getServiceReference(CDIOSGiContainerFactory.class.getName());
-        if (factory == null) {
-            throw new RuntimeException("Service unavailbale");
-        }
         context.addBundleListener(this);
         context.addServiceListener(this);
-        for (Bundle bundle : context.getBundles()) {
-            if (!containers.containsKey(bundle.getBundleId())) {
-                if (bundle.getState() >= Bundle.STARTING) {
-                    CDIOSGiContainer container = factory.getContainer(bundle);
-                    containers.putIfAbsent(bundle.getBundleId(), container);
-                }
-            }
-        }
-        for (CDIOSGiContainer container : containers.values()) {
-            boolean started = container.initialize();
-            if (!started) {
-                containers.remove(context.getBundle().getBundleId());
-            }
-        }
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
-        for (CDIOSGiContainer container : containers.values()) {
-            container.shutdown();
-        }
-        SingletonProvider.reset();
     }
 
     @Override
     public void bundleChanged(BundleEvent event) {
-        
-        if (event.getType() == BundleEvent.STARTED) {
-            if (!containers.containsKey(event.getBundle().getBundleId())) {
-                CDIOSGiContainer container =  factory.getContainer(event.getBundle());
-                CDIOSGiContainer maybeExistingContainer = containers.putIfAbsent(
-                        event.getBundle().getBundleId(), container);
-                if (maybeExistingContainer == null) {
-                    CDIOSGiContainer existingContainer = containers.get(event.getBundle().getBundleId());
-                    boolean started = existingContainer.initialize();
-                    if (!started) {
-                        containers.remove(event.getBundle().getBundleId());
-                    }
-                }
-            }
-        }
-        if (BundleEvent.STOPPED == event.getType()) {
-            if (containers.containsKey(event.getBundle().getBundleId())) {
-                CDIOSGiContainer container = containers.get(event.getBundle().getBundleId());
-                container.shutdown();
-                containers.remove(event.getBundle().getBundleId());
-            }
-        }
-        for (CDIOSGiContainer container : containers.values()) {
-            if (container != null) {
-                container.getContainer().event().select(BundleEvent.class).fire(event);
+        BundleContext context = event.getBundle().getBundleContext();
+        ServiceReference[] references = findReferences(context, Event.class);
+
+        if (references != null) {
+            for (ServiceReference reference : references) {
+                Event<Object> e = (Event<Object>) context.getService(reference);
+                e.select(BundleEvent.class).fire(event);
             }
         }
     }
 
+    private ServiceReference[] findReferences(BundleContext context, Class<?> type) {
+        ServiceReference[] references = null;
+        try {
+            references = context.getServiceReferences(type.getName(), null);
+        } catch (InvalidSyntaxException e) {
+            // Ignored
+        }
+        return references;
+    }
+
     @Override
     public void serviceChanged(ServiceEvent event) {
-        for (CDIOSGiContainer container : containers.values()) {
-            container.getContainer().event().select(ServiceEvent.class).fire(event);
+        BundleContext context = event.getServiceReference().getBundle().getBundleContext();
+        ServiceReference[] references = findReferences(context, Event.class);
+
+        if (references != null) {
+            for (ServiceReference reference : references) {
+                Event<Object> e = (Event<Object>) context.getService(reference);
+                e.select(ServiceEvent.class).fire(event);
+            }
         }
     }
 }
--- weld-osgi-impl/src/main/java/org/jboss/weld/environment/osgi/integration/IntegrationActivator.java
+++ weld-osgi-impl/src/main/java/org/jboss/weld/environment/osgi/integration/IntegrationActivator.java
@@ -1,8 +1,15 @@
 package org.jboss.weld.environment.osgi.integration;
 
+import org.jboss.weld.bootstrap.api.SingletonProvider;
+import org.jboss.weld.bootstrap.api.helpers.IsolatedStaticSingletonProvider;
+import org.jboss.weld.environment.osgi.api.integration.CDIOSGiContainer;
 import org.jboss.weld.environment.osgi.api.integration.CDIOSGiContainerFactory;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
+import org.osgi.framework.*;
+
+import javax.enterprise.event.Event;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.BeanManager;
+import java.util.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,17 +18,110 @@ import org.osgi.framework.BundleContext;
  * Time: 22:27
  * To change this template use File | Settings | File Templates.
  */
-public class IntegrationActivator implements BundleActivator {
+public class IntegrationActivator implements BundleActivator, BundleListener {
+
+
+    private WeldFactory factory;
+
+    private Map<Long, Holder> managed;
+
     @Override
     public void start(BundleContext context) throws Exception {
-        WeldFactory factory = new WeldFactory();
+
+        // Init the SingletonProvider
+        SingletonProvider.initialize(new IsolatedStaticSingletonProvider());
+
+        managed = new HashMap<Long, Holder>();
+
+        factory = new WeldFactory();
         context.registerService(CDIOSGiContainerFactory.class.getName(),
-                                factory,
-                                null);
+                factory,
+                null);
+
+        for (Bundle bundle : context.getBundles()) {
+            if (Bundle.ACTIVE == bundle.getState()) {
+                startManagement(bundle);
+            }
+        }
+
+        context.addBundleListener(this);
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
-        //To change body of implemented methods use File | Settings | File Templates.
+        for (Bundle bundle : context.getBundles()) {
+            Holder holder = managed.get(bundle.getBundleId());
+            if (holder != null) {
+                stopManagement(holder.bundle);
+            }
+        }
+
+        SingletonProvider.reset();
+    }
+
+    @Override
+    public void bundleChanged(BundleEvent event) {
+        switch (event.getType()) {
+            case BundleEvent.STARTED:
+                startManagement(event.getBundle());
+                break;
+            case BundleEvent.STOPPING:
+                stopManagement(event.getBundle());
+                break;
+        }
+    }
+
+    private void stopManagement(Bundle bundle) {
+        Holder holder = managed.get(bundle.getBundleId());
+        if (holder != null) {
+
+            Collection<ServiceRegistration> regs = holder.registrations;
+            for (ServiceRegistration reg : regs) {
+                reg.unregister();
+            }
+
+            holder.container.shutdown();
+            managed.remove(bundle.getBundleId());
+        }
+    }
+
+    private void startManagement(Bundle bundle) {
+        System.out.println("Starting management for bundle " + bundle);
+        CDIOSGiContainer container = factory.getContainer(bundle);
+        container.initialize();
+
+        if (container.isStarted()) {
+
+            Collection<ServiceRegistration> regs = new ArrayList<ServiceRegistration>();
+
+            BundleContext bundleContext = bundle.getBundleContext();
+            regs.add(
+            bundleContext.registerService(Event.class.getName(),
+                                          container.getContainer().event(),
+                                          null));
+
+            regs.add(
+                    bundleContext.registerService(BeanManager.class.getName(),
+                            container.getContainer().event(),
+                            null));
+
+            regs.add(
+                    bundleContext.registerService(Instance.class.getName(),
+                            container.getContainer().event(),
+                            null));
+
+            Holder holder = new Holder();
+            holder.container = container;
+            holder.registrations = regs;
+            holder.bundle = bundle;
+            managed.put(bundle.getBundleId(), holder);
+
+        }
+    }
+
+    private static class Holder {
+        Bundle bundle;
+        CDIOSGiContainer container;
+        Collection<ServiceRegistration> registrations;
     }
 }
