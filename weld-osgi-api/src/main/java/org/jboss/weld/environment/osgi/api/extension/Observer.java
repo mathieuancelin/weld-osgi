3045f33bbfd1483e3c2a8177d96d886bb9dbdfa1
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-01-29 13:37:23 +0100

    Annotation can be used on method

--- weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Observer.java
+++ weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Observer.java
@@ -1,14 +1,14 @@
 package org.jboss.weld.environment.osgi.api.extension;
 
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 import javax.interceptor.InterceptorBinding;
-import static java.lang.annotation.ElementType.TYPE;
 
 @InterceptorBinding
-@Target(TYPE)
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Observer {
 
