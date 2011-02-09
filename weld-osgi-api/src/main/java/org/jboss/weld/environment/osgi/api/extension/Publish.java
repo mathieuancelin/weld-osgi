b7b27d5389f737c68edc8d7d35cb8fad99171c18
Guillaume Sauthier <guillaume.sauthier@gmail.com>
2011-02-09 22:59:52 +0100

    Add some necessary annotations to be a valid @Qualifier

--- weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Publish.java
+++ weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Publish.java
@@ -1,6 +1,9 @@
 package org.jboss.weld.environment.osgi.api.extension;
 
-import static java.lang.annotation.ElementType.TYPE;
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+
+import static java.lang.annotation.ElementType.*;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,8 +13,10 @@ import java.lang.annotation.Target;
  *
  * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
  */
-@Target({ TYPE })
+@Qualifier
+@Target({FIELD,METHOD,PARAMETER,TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Publish {
+    @Nonbinding
     public Class[] contracts() default {};
 }
