2cd5c719ec3bb4daaaaf1ffeab0e1c0c21209d78
Guillaume Sauthier <guillaume.sauthier@gmail.com>
2011-02-09 23:00:22 +0100

    Add a line easy to uncomment to activate remote debug

--- build-weld-osgi.sh
+++ build-weld-osgi.sh
@@ -2,7 +2,8 @@
 #
 
 clear;
+#export DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,address=8000"
 #mvn clean install;
 cd ./container/target/osgi-cdi-container-1.0-SNAPSHOT-all/osgi-cdi-container-1.0-SNAPSHOT/;
-java -jar bin/felix.jar;
+java $DEBUG_OPTS -jar bin/felix.jar;
 
