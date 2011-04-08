5af62c8ed29bac3771bf4d8172f0b48b3d36c9b4
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-04-08 12:20:37 +0200

    Add a bean to holder current bundle and bundle context information

---
+++ weld-osgi-impl/src/main/java/org/jboss/weld/environment/osgi/extension/services/BundleHolder.java
@@ -0,0 +1,33 @@
+package org.jboss.weld.environment.osgi.extension.services;
+
+import javax.enterprise.context.ApplicationScoped;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+/**
+ *
+ * @author mathieuancelin
+ */
+@ApplicationScoped
+public class BundleHolder {
+
+    private Bundle bundle;
+    
+    private BundleContext context;
+
+    public Bundle getBundle() {
+        return bundle;
+    }
+
+    public void setBundle(Bundle bundle) {
+        this.bundle = bundle;
+    }
+
+    public BundleContext getContext() {
+        return context;
+    }
+
+    public void setContext(BundleContext context) {
+        this.context = context;
+    }
+}
