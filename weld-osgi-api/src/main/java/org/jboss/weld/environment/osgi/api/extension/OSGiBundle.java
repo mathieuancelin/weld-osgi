8149e2b07c2ff9426dc334bf9155a19a59a9a09d
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-04-11 11:08:11 +0200

    Add bundle injectioin

---
+++ weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/OSGiBundle.java
@@ -0,0 +1,21 @@
+package org.jboss.weld.environment.osgi.api.extension;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+
+/**
+ *
+ * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OSGiBundle {
+    @Nonbinding
+    String symbolicName() default "";
+
+    @Nonbinding
+    String version() default "";
+}
--- weld-osgi-impl/src/main/java/org/jboss/weld/environment/osgi/extension/services/WeldOSGiProducer.java
+++ weld-osgi-impl/src/main/java/org/jboss/weld/environment/osgi/extension/services/WeldOSGiProducer.java
@@ -1,14 +1,19 @@
 package org.jboss.weld.environment.osgi.extension.services;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Proxy;
 import java.util.Dictionary;
 import java.util.Set;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import org.jboss.weld.environment.osgi.api.extension.BundleHeaders;
+import org.jboss.weld.environment.osgi.api.extension.OSGiBundle;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Version;
 
 /**
  * Producers for Specific injected types;
@@ -20,6 +25,24 @@ public class WeldOSGiProducer {
     @Produces
     public Bundle getBundle(BundleHolder holder, InjectionPoint p) {
         return holder.getBundle();
+    }
+
+    @Produces @OSGiBundle
+    public Bundle getSpecificBundle(BundleHolder holder, InjectionPoint p) {
+        Set<Annotation> qualifiers = p.getQualifiers();
+        OSGiBundle bundle = null;
+        for (Annotation qualifier : qualifiers) {
+            if (qualifier.annotationType().equals(OSGiBundle.class)) {
+                bundle = (OSGiBundle) qualifier;
+                break;
+            }
+        }
+        if (bundle.symbolicName().equals("")) {
+            return getBundle(holder, p);
+        }
+        return (Bundle) Proxy.newProxyInstance(getClass().getClassLoader(),
+                new Class[] {Bundle.class},
+                new BundleHandler(bundle.symbolicName(), bundle.version(), holder.getContext()));
     }
 
     @Produces
@@ -61,5 +84,50 @@ public class WeldOSGiProducer {
             return null; // not cool at all ...
         }
         return (String) holder.getBundle().getHeaders().get(headers.value());
+    }
+
+    private static class BundleHandler implements InvocationHandler {
+
+        private final String symbolicName;
+
+        private Version version;
+
+        private final BundleContext context;
+
+        public BundleHandler(String symbolicName, String version, BundleContext context) {
+            this.symbolicName = symbolicName;
+            this.context = context;
+            if (!version.equals("")) {
+                this.version = new Version(version);
+            }
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Bundle bundle = null;
+            Bundle[] bundles = context.getBundles();
+            if (bundles != null) {
+                for (Bundle b : bundles) {
+                    if (bundle != null) {
+                        if (b.getSymbolicName().equals(symbolicName)) {
+                            if (version != null) {
+                                if (version.equals(b.getVersion())) {
+                                    bundle = b;
+                                    break;
+                                }
+                            } else {
+                                bundle = b;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            if (bundle == null) {
+                System.out.println("Bundle " + symbolicName + " is unavailable.");
+                return null;
+            }
+            return method.invoke(bundle, args);
+        }
     }
 }
