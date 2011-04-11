6929b5d25f7fcdd3df94757f66749688c9725f92
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-04-11 16:15:29 +0200

    Add an embedded API

---
+++ weld-osgi-impl/src/main/java/org/jboss/weld/environment/osgi/integration/WeldEmbedded.java
@@ -0,0 +1,88 @@
+package org.jboss.weld.environment.osgi.integration;
+
+import java.awt.Event;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.BeanManager;
+import org.jboss.weld.environment.osgi.extension.ExtensionActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ *
+ * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
+ */
+public class WeldEmbedded {
+
+    private final Weld weld;
+    private final ExtensionActivator activator;
+    private final BundleContext context;
+    private Collection<ServiceRegistration> regs = new ArrayList<ServiceRegistration>();
+
+    private WeldEmbedded(Weld weld, ExtensionActivator activator, BundleContext context) {
+        this.weld = weld;
+        this.activator = activator;
+        this.context = context;
+    }
+
+    public static WeldEmbedded startFor(BundleContext context) throws Exception {
+        boolean set = BundleSingletonProvider.currentBundle.get() != null;
+        BundleSingletonProvider.currentBundle.set(context.getBundle().getBundleId());
+        WeldEmbedded embedded =
+                new WeldEmbedded(new Weld(context.getBundle()),
+                new ExtensionActivator(), context);
+        try {
+            embedded.regs.add(
+                    context.registerService(Event.class.getName(),
+                    embedded.weld.getEvent(),
+                    null));
+
+            embedded.regs.add(
+                    context.registerService(BeanManager.class.getName(),
+                    embedded.weld.getBeanManager(),
+                    null));
+
+            embedded.regs.add(
+                    context.registerService(Instance.class.getName(),
+                    embedded.weld.getInstance(),
+                    null));
+        } catch (Throwable t) {
+            // Ignore
+        }
+        embedded.activator.start(context);
+        if (!set) {
+            BundleSingletonProvider.currentBundle.remove();
+        }
+        return embedded;
+    }
+
+    public void shutdown() throws Exception {
+        boolean set = BundleSingletonProvider.currentBundle.get() != null;
+        BundleSingletonProvider.currentBundle.set(context.getBundle().getBundleId());
+        activator.stop(context);
+        for (ServiceRegistration reg : regs) {
+            try {
+                reg.unregister();
+            } catch (IllegalStateException e) {
+                // Ignore
+            }
+        }
+        weld.shutdown();
+        if (!set) {
+            BundleSingletonProvider.currentBundle.remove();
+        }
+    }
+
+    public Event event() {
+        return weld.getInstance().select(Event.class).get();
+    }
+
+    public BeanManager beanManager() {
+        return weld.getBeanManager();
+    }
+
+    public Instance<Object> instance() {
+        return weld.getInstance();
+    }
+}
