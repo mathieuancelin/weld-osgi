6a1aa1a5bec6a1520dc3520df474aea1bea25156
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-04-12 15:59:04 +0200

    Change target for Filter annotation

--- weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Filter.java
+++ weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Filter.java
@@ -1,10 +1,8 @@
 package org.jboss.weld.environment.osgi.api.extension;
 
-import static java.lang.annotation.ElementType.PARAMETER;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import javax.inject.Qualifier;
 
 /**
@@ -12,7 +10,6 @@ import javax.inject.Qualifier;
  * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
  */
 @Qualifier
-@Target({ PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Filter {
     String value();
