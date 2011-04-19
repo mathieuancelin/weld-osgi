61bc7ad0a75d3898cdf60e11dc31e3ca37f42691
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-04-19 16:08:18 +0200

    Used Provider API

--- weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Service.java
+++ weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Service.java
@@ -1,11 +1,11 @@
 package org.jboss.weld.environment.osgi.api.extension;
 
+import javax.inject.Provider;
+
 /**
  *
  * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
  */
-public interface Service<T> {
-
-    T get();
+public interface Service<T> extends Provider<T> {
 
 }
