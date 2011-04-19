fa9474e0707b5704a8abd72f556df1cf3e47a6c1
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-04-19 16:19:30 +0200

    Undo

--- weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Service.java
+++ weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Service.java
@@ -1,11 +1,10 @@
 package org.jboss.weld.environment.osgi.api.extension;
 
-import javax.inject.Provider;
-
 /**
  *
  * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
  */
-public interface Service<T> extends Provider<T> {
+public interface Service<T> {
 
+    T get();
 }
