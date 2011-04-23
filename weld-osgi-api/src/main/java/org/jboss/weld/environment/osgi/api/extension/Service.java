3172d15afc1a1e5150ec36f267e06128dfc4e0fc
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-04-23 12:11:51 +0200

    Add last methods

--- weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Service.java
+++ weld-osgi-api/src/main/java/org/jboss/weld/environment/osgi/api/extension/Service.java
@@ -1,18 +1,22 @@
 package org.jboss.weld.environment.osgi.api.extension;
 
+import java.lang.annotation.Annotation;
+import javax.enterprise.util.TypeLiteral;
+
 /**
  *
  * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
  */
+// should extends Instance<T> but weld doesn't seems to like it
 public interface Service<T> extends Iterable<T> {
 
     T get();
 
-//    Service<T> select(Annotation... qualifiers);
-//
-//    <U extends T> Service<U> select(Class<U> subtype, Annotation... qualifiers);
-//
-//    <U extends T> Service<U> select(TypeLiteral<U> subtype, Annotation... qualifiers);
+    Service<T> select(Annotation... qualifiers);
+
+    <U extends T> Service<U> select(Class<U> subtype, Annotation... qualifiers);
+
+    <U extends T> Service<U> select(TypeLiteral<U> subtype, Annotation... qualifiers);
 
     boolean isUnsatisfied();
 
