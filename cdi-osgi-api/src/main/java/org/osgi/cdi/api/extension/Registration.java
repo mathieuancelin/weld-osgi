93a161fbea424fcb9adb7a5dcc18f82d98c23cb7
arcane86 <clochard.matthieu@gmail.com>
2011-05-04 13:38:45 +0200

    Redo specification modifications on Registration<T> (code issue now).

--- cdi-osgi-api/src/main/java/org/osgi/cdi/api/extension/Registration.java
+++ cdi-osgi-api/src/main/java/org/osgi/cdi/api/extension/Registration.java
@@ -3,6 +3,9 @@ package org.osgi.cdi.api.extension;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
+import javax.enterprise.util.TypeLiteral;
+import java.lang.annotation.Annotation;
+
 /**
  *
  * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
@@ -12,5 +15,9 @@ public interface Registration<T> extends Iterable<Registration<T>> {
 
     void unregister();
     <T> Service<T> getServiceReference();
+    Service<T> select(Annotation... qualifiers);
+    Service<T> select(String filter);
+    <U extends T> Service<U> select(Class<U> subtype, Annotation... qualifiers);
+    <U extends T> Service<U> select(Class<U> subtype, String filter);
     int size();
 }
