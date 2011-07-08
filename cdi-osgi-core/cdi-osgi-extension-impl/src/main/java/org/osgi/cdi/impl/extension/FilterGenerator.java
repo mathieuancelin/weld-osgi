30d11e864bd6c3d8eb8a5041865734b1b63ac037
Matthieu Clochard <clochard.matthieu@gmail.com>
2011-07-08 11:38:10 +0200

    Test and improve FilterGenerator.Review specifications accordingly

--- cdi-osgi-core/cdi-osgi-extension-impl/src/main/java/org/osgi/cdi/impl/extension/FilterGenerator.java
+++ cdi-osgi-core/cdi-osgi-extension-impl/src/main/java/org/osgi/cdi/impl/extension/FilterGenerator.java
@@ -11,16 +11,19 @@
  */
 package org.osgi.cdi.impl.extension;
 
-import org.osgi.cdi.api.extension.annotation.Filter;
-import org.osgi.cdi.api.extension.annotation.Property;
-import org.osgi.cdi.api.extension.annotation.Publish;
+import org.osgi.cdi.api.extension.annotation.*;
 
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Default;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.enterprise.util.Nonbinding;
 import javax.inject.Qualifier;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Helper class for generating {@link Filter} qualifier from various sources (other {@link Filter}, {@link Qualifier}, {@link String} ...).
@@ -39,13 +42,13 @@ public class FilterGenerator {
             return new OSGiFilterQualifierType("");
         }
         StringBuilder stringBuilder = new StringBuilder();
-        if (tokens.size() > 2){
+        if (tokens.size() > 1){
             stringBuilder.append("(&");
         }
         for (String token : tokens) {
             stringBuilder.append(token);
         }
-        if (tokens.size() > 2){
+        if (tokens.size() > 1){
             stringBuilder.append(")");
         }
         return new OSGiFilterQualifierType(stringBuilder.toString());
@@ -57,6 +60,10 @@ public class FilterGenerator {
 
     public static Filter makeFilter(List<Annotation> annotations) {
         return make(tokenize(annotations));
+    }
+
+    public static Filter normalize(Filter filter) {
+        return make(tokenize(filter));
     }
 
     public static Filter makeFilter(Publish publish) {
@@ -84,18 +91,14 @@ public class FilterGenerator {
     
     public static Filter makeFilter(Filter old, String filter) {
         Set<String> tokens = new HashSet<String>();
-        if(old != null && old.value() != null && old.value().length() > 0) {
-            tokens.add(old.value());
-        }
+        tokens.addAll(tokenize(old));
         tokens.add(filter);
         return make(tokens);
     }
 
     public static Filter makeFilter(Filter old, Collection<Annotation> annotations) {
         Set<String> tokens = new HashSet<String>();
-        if(old != null && old.value() != null && old.value().length() > 0) {
-            tokens.add(old.value());
-        }
+        tokens.addAll(tokenize(old));
         tokens.addAll(tokenize(annotations));
         return make(tokens);
     }
@@ -103,7 +106,15 @@ public class FilterGenerator {
     private static Set<String> tokenize(Publish publish) {
         Set<String> result = new HashSet<String>();
         for(Property property : publish.properties()) {
-            result.add("(" + property.name() + "=" + property.value() + ")");
+            result.add("(" + property.name().toLowerCase() + "=" + property.value() + ")");
+        }
+        return result;
+    }
+
+    private static Set<String> tokenize(Filter filter) {
+        Set<String> result = new HashSet<String>();
+        if(filter != null && filter.value() != null && filter.value().length() > 0) {
+            result.add(filter.value());
         }
         return result;
     }
@@ -113,19 +124,34 @@ public class FilterGenerator {
         String current = "";
         for (Annotation annotation : annotations) {
             if(annotation.annotationType().isAnnotationPresent(Qualifier.class)) {
-                if (!annotation.annotationType().equals(Filter.class)) {
-                    for (Method m : annotation.annotationType().getDeclaredMethods()) {
-                        if (!m.isAnnotationPresent(Nonbinding.class)) {
-                            try {
-                                Object value = m.invoke(annotation);
-                                if (value == null) {
-                                    value = m.getDefaultValue();
+                if (annotation.annotationType().equals(Filter.class)) {
+                    result.addAll(tokenize((Filter)annotation));
+                } else if(!annotation.annotationType().equals(Required.class)
+                        && !annotation.annotationType().equals(OSGiService.class)
+                        && !annotation.annotationType().equals(Default.class)
+                        && !annotation.annotationType().equals(Any.class)) {
+                    if (annotation.annotationType().getDeclaredMethods().length > 0) {
+                        for (Method m : annotation.annotationType().getDeclaredMethods()) {
+                            if (!m.isAnnotationPresent(Nonbinding.class)) {
+                                try {
+                                    Object value = m.invoke(annotation);
+                                    if (value == null) {
+                                        value = m.getDefaultValue();
+                                        if(value == null) {
+                                            value = "*";
+                                        }
+                                    }
+                                    current = "(" + annotation.annotationType().getSimpleName().toLowerCase()
+                                              + "." + m.getName().toLowerCase()
+                                              + "=" + value + ")";
+                                    result.add(current);
+                                } catch (Throwable t) {// inaccessible property, skip
                                 }
-                                current = "(" + annotation.annotationType().getSimpleName() + "." + m.getName() + "=" + value + ")";
-                                result.add(current);
-                            } catch (Throwable t) {// inaccessible property, skip
                             }
                         }
+                    } else {
+                        current = "(" + annotation.annotationType().getSimpleName().toLowerCase() + "=*)";
+                        result.add(current);
                     }
                 }
             }
---
+++ cdi-osgi-core/cdi-osgi-extension-impl/src/test/java/org/osgi/cdi/test/extension/FilterGeneratorTest.java
@@ -0,0 +1,188 @@
+package org.osgi.cdi.test.extension;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.osgi.cdi.impl.extension.FilterGenerator;
+
+import javax.enterprise.util.AnnotationLiteral;
+import javax.inject.Qualifier;
+import java.lang.annotation.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.lang.annotation.ElementType.*;
+
+public class FilterGeneratorTest {
+
+    @Test
+    public void filterTest() {
+
+        Assert.assertEquals("makeFilter() was wrong", "", FilterGenerator.makeFilter().value());
+
+        String tokA = "token1", tokB = "token2", tokC = "token2";
+        Set<String> set1 = new HashSet<String>(),
+                set2 = new HashSet<String>(),
+                set3 = new HashSet<String>(),
+                set0 = new HashSet<String>();
+        set1.add(tokA); set2.add(tokA); set3.add(tokA);
+        set2.add(tokB); set3.add(tokB);
+        set3.add(tokC);
+
+        Assert.assertEquals("make(set0) was wrong", "", FilterGenerator.make(set0).value());
+        Assert.assertEquals("make(set1) was wrong","token1", FilterGenerator.make(set1).value());
+        Assert.assertEquals("make(set2) was wrong","(&token1token2)", FilterGenerator.make(set2).value());
+        Assert.assertEquals("make(set3) was wrong","(&token1token2)", FilterGenerator.make(set3).value());
+
+        Assert.assertEquals("makeFilter(tokA) was wrong","token1", FilterGenerator.makeFilter(tokA).value());
+        Assert.assertEquals("makeFilter(tokA + tokB) was wrong","token1token2", FilterGenerator.makeFilter(tokA + tokB).value());
+
+        NonQualifierAnnotation nonQualifierAnnotation = new NonQualifierAnnotationLiteral();
+        QualifierAnnotation qualifierAnnotation = new QualifierAnnotationLiteral();
+        QualifierWithValueAnnotation qualifierWithValueAnnotation = new QualifierWithValueAnnotationLiteral("QWVA");
+        QualifierWithDefaultValueAnnotation qualifierWithDefaultValueAnnotation = new QualifierWithDefaultValueAnnotationLiteral();
+        QualifierWithValuesAnnotation qualifierWithValuesAnnotation = new QualifierWithValuesAnnotationLiteral("QWVsA");
+
+        List<Annotation> list1 = new ArrayList<Annotation>(),
+                list2 = new ArrayList<Annotation>(),
+                list3 = new ArrayList<Annotation>(),
+                list4 = new ArrayList<Annotation>(),
+                list5 = new ArrayList<Annotation>(),
+                list6 = new ArrayList<Annotation>(),
+                list7 = new ArrayList<Annotation>(),
+                list8 = new ArrayList<Annotation>(),
+                list0 = new ArrayList<Annotation>();
+        list1.add(nonQualifierAnnotation);
+        list2.add(qualifierAnnotation);
+        list3.add(qualifierWithValueAnnotation);
+        list4.add(qualifierWithDefaultValueAnnotation);
+        list5.add(qualifierWithValuesAnnotation);
+        list6.add(qualifierWithValueAnnotation); list6.add(qualifierWithValueAnnotation);
+        list7.add(qualifierWithValueAnnotation); list7.add(qualifierWithDefaultValueAnnotation);
+        list8.add(nonQualifierAnnotation); list8.add(qualifierAnnotation); list8.add(qualifierWithValueAnnotation);
+        list8.add(qualifierWithDefaultValueAnnotation); list8.add(qualifierWithValuesAnnotation);
+
+        Assert.assertEquals("makeFilter(list0) was wrong","", FilterGenerator.makeFilter(list0).value());
+        Assert.assertEquals("makeFilter(list1) was wrong","", FilterGenerator.makeFilter(list1).value());
+        Assert.assertEquals("makeFilter(list2) was wrong","(qualifierannotation=*)", FilterGenerator.makeFilter(list2).value());
+        Assert.assertEquals("makeFilter(list3) was wrong","(qualifierwithvalueannotation.value=QWVA)", FilterGenerator.makeFilter(list3).value());
+        Assert.assertEquals("makeFilter(list4) was wrong","(qualifierwithdefaultvalueannotation.value=default)", FilterGenerator.makeFilter(list4).value());
+        Assert.assertEquals("makeFilter(list5) was wrong","(&(qualifierwithvaluesannotation.value=QWVsA)(qualifierwithvaluesannotation.name=name))", FilterGenerator.makeFilter(list5).value());
+        Assert.assertEquals("makeFilter(list6) was wrong","(qualifierwithvalueannotation.value=QWVA)", FilterGenerator.makeFilter(list6).value());
+        Assert.assertEquals("makeFilter(list7) was wrong","(&(qualifierwithvalueannotation.value=QWVA)(qualifierwithdefaultvalueannotation.value=default))", FilterGenerator.makeFilter(list7).value());
+        Assert.assertEquals("makeFilter(list8) was wrong","(&(qualifierwithvaluesannotation.value=QWVsA)(qualifierwithvaluesannotation.name=name)(qualifierannotation=*)(qualifierwithvalueannotation.value=QWVA)(qualifierwithdefaultvalueannotation.value=default))", FilterGenerator.makeFilter(list8).value());
+    }
+
+    @Target({TYPE, METHOD, PARAMETER, FIELD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    public @interface NonQualifierAnnotation {
+    }
+
+    public class NonQualifierAnnotationLiteral
+            extends AnnotationLiteral<NonQualifierAnnotation>
+            implements NonQualifierAnnotation {
+    }
+
+    @Target({TYPE, METHOD, PARAMETER, FIELD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @Qualifier
+    public @interface QualifierAnnotation {
+    }
+
+    public class QualifierAnnotationLiteral
+            extends AnnotationLiteral<QualifierAnnotation>
+            implements QualifierAnnotation {
+    }
+
+    @Target({TYPE, METHOD, PARAMETER, FIELD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @Qualifier
+    public @interface QualifierWithValueAnnotation {
+        String value();
+    }
+
+    public class QualifierWithValueAnnotationLiteral
+            extends AnnotationLiteral<QualifierWithValueAnnotation>
+            implements QualifierWithValueAnnotation {
+        String value;
+
+        public QualifierWithValueAnnotationLiteral(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String value() {
+            return value;
+        }
+    }
+
+    @Target({TYPE, METHOD, PARAMETER, FIELD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @Qualifier
+    public @interface QualifierWithDefaultValueAnnotation {
+        String value() default "default";
+    }
+
+    public class QualifierWithDefaultValueAnnotationLiteral
+            extends AnnotationLiteral<QualifierWithDefaultValueAnnotation>
+            implements QualifierWithDefaultValueAnnotation {
+        String value;
+
+        public QualifierWithDefaultValueAnnotationLiteral() {
+            this.value = "default";
+        }
+
+        public QualifierWithDefaultValueAnnotationLiteral(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String value() {
+            return value;
+        }
+    }
+
+    @Target({TYPE, METHOD, PARAMETER, FIELD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @Qualifier
+    public @interface QualifierWithValuesAnnotation {
+        String value() default "default";
+        String name() default "name";
+    }
+
+    public class QualifierWithValuesAnnotationLiteral
+            extends AnnotationLiteral<QualifierWithValuesAnnotation>
+            implements QualifierWithValuesAnnotation {
+        String value;
+        String name;
+
+        public QualifierWithValuesAnnotationLiteral() {
+            this.value = "default";
+        }
+
+        public QualifierWithValuesAnnotationLiteral(String value) {
+            this.value = value;
+        }
+
+        public QualifierWithValuesAnnotationLiteral(String value, String name) {
+            this.value = value;
+            this.name = name;
+        }
+
+        @Override
+        public String value() {
+            return value;
+        }
+
+        @Override
+        public String name() {
+            return name;
+        }
+    }
+}
