68237ea0539b9f7dd22fcb22eafdb192f98c508f
Matthieu Clochard <clochard.matthieu@gmail.com>
2011-07-26 15:05:33 +0200

    Use javassist to scan auto-publishable classes

--- cdi-osgi-core/cdi-osgi-extension-impl/src/main/java/org/osgi/cdi/impl/integration/ServicePublisher.java
+++ cdi-osgi-core/cdi-osgi-extension-impl/src/main/java/org/osgi/cdi/impl/integration/ServicePublisher.java
@@ -11,6 +11,10 @@
  */
 package org.osgi.cdi.impl.integration;
 
+import javassist.ClassClassPath;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.NotFoundException;
 import org.osgi.cdi.api.extension.annotation.Property;
 import org.osgi.cdi.api.extension.annotation.Publish;
 import org.osgi.cdi.impl.extension.CDIOSGiExtension;
@@ -53,71 +57,80 @@ public class ServicePublisher {
     public void registerAndLaunchComponents() {
         logger.info("Registering/Starting OSGi Service for bundle {}", bundle.getSymbolicName());
 
-        Class<?> clazz;
-        for (String className : classes) {
-            logger.trace("Scanning class {}", className);
-
-            try {
-                clazz = bundle.loadClass(className);
-            } catch (Exception e) {//inaccessible class
-                logger.warn("Class {} cannot be load", className);
-                continue;
-            }
-            //is an auto-publishable class?
-            if (clazz.isAnnotationPresent(Publish.class)) {
-                logger.debug("Found a new auto-published service class {}", clazz);
-                Object service = null;
-                InstanceHolder instanceHolder = instance.select(InstanceHolder.class).get();
-                List<Annotation> qualifiers = getQualifiers(clazz);
-                try {
-                    Instance instance = instanceHolder.select(clazz, qualifiers.toArray(new Annotation[qualifiers.size()]));
-                    service = instance.get();
-                    logger.trace("Service instance generated");
-                } catch (Throwable e) {
-                    logger.error("Unable to instantiate the service for class {}, CDI return this error: {}", clazz, e);
-                    throw new RuntimeException(e);
-                }
-                publish(clazz, service, qualifiers);
-            }
-        }
-
-// TODO Use javassist in order to reduce the number of loaded class
-// Currently unable to use javassist: if javassist is accessible in class path, Weld returns a java.lang.ClassCastException
-//        ClassPool classPool = new ClassPool();
-//        CtClass ctClass = null;
 //        Class<?> clazz;
 //        for (String className : classes) {
 //            logger.trace("Scanning class {}", className);
-
+//
 //            try {
-//                ctClass = classPool.get(className);
-//                if(ctClass.getAnnotation(Publish.class) != null) {
-//                    logger.debug("Found a new auto-published service class {}", className);
-//                    clazz = bundle.loadClass(className);
-//                    Object service = null;
-//                    InstanceHolder instanceHolder = instance.select(InstanceHolder.class).get();
-//                    List<Annotation> qualifiers = getQualifiers(clazz);
-//                    try {
-//                        Instance instance = instanceHolder.select(clazz, qualifiers.toArray(new Annotation[qualifiers.size()]));
-//                        service = instance.get();
-//                        logger.trace("Service instance generated");
-//                    } catch (Throwable e) {
-//                        logger.error("Unable to instantiate the service for class {}, CDI return this error: {}", clazz, e);
-//                        throw new RuntimeException(e);
-//                    }
-//                    publish(clazz, service, qualifiers);
+//                clazz = bundle.loadClass(className);
+//            } catch (Exception e) {//inaccessible class
+//                logger.warn("Class {} cannot be load", className);
+//                continue;
+//            }
+//            //is an auto-publishable class?
+//            if (clazz.isAnnotationPresent(Publish.class)) {
+//                logger.debug("Found a new auto-published service class {}", clazz);
+//                Object service = null;
+//                InstanceHolder instanceHolder = instance.select(InstanceHolder.class).get();
+//                List<Annotation> qualifiers = getQualifiers(clazz);
+//                try {
+//                    Instance instance = instanceHolder.select(clazz, qualifiers.toArray(new Annotation[qualifiers.size()]));
+//                    service = instance.get();
+//                    logger.trace("Service instance generated");
+//                } catch (Throwable e) {
+//                    logger.error("Unable to instantiate the service for class {}, CDI return this error: {}", clazz, e);
+//                    throw new RuntimeException(e);
 //                }
-//            } catch (NotFoundException e) {//inaccessible class
-//                logger.warn("Class file {} is inaccessible", className);
-//            } catch (ClassNotFoundException e) {
-//                logger.warn("Class file {} cannot be read/load", className);
-//            } finally {
-//                if (ctClass != null) {
-//                    ctClass.detach();
-//                    ctClass = null;
-//                }
+//                publish(clazz, service, qualifiers);
 //            }
 //        }
+
+// TODO Use javassist in order to reduce the number of loaded class
+// Currently unable to use javassist: if javassist is accessible in class path, Weld returns a java.lang.ClassCastException
+
+        if (!classes.isEmpty()) {
+            ClassPool classPool = new ClassPool();
+            try {
+                classPool.appendClassPath(new ClassClassPath(bundle.loadClass(classes.iterator().next())));
+            } catch (ClassNotFoundException e) {
+                logger.warn("Bundle {} is inaccessible", bundle);
+            }
+            CtClass ctClass = null;
+            Class<?> clazz;
+            for (String className : classes) {
+                logger.trace("Scanning class {}", className);
+
+                try {
+                    ctClass = classPool.get(className);
+                    if (ctClass.getAnnotation(Publish.class) != null) {
+                        logger.debug("Found a new auto-published service class {}", className);
+                        clazz = bundle.loadClass(className);
+                        Object service = null;
+                        InstanceHolder instanceHolder = instance.select(InstanceHolder.class).get();
+                        List<Annotation> qualifiers = getQualifiers(clazz);
+                        try {
+                            Instance instance = instanceHolder.select(clazz, qualifiers.toArray(new Annotation[qualifiers.size()]));
+                            service = instance.get();
+                            logger.trace("Service instance generated");
+                        } catch (Throwable e) {
+                            logger.error("Unable to instantiate the service for class {}, CDI return this error: {}", clazz, e);
+                            throw new RuntimeException(e);
+                        }
+                        publish(clazz, service, qualifiers);
+                    }
+                } catch (NotFoundException e) {//inaccessible class
+                    logger.warn("Class file {} is inaccessible", className);
+                } catch (ClassNotFoundException e) {
+                    logger.warn("Class file {} cannot be read/load", className);
+                } finally {
+                    if (ctClass != null) {
+                        ctClass.detach();
+                        ctClass = null;
+                    }
+                }
+            }
+        }
+
     }
 
     private void publish(Class<?> clazz, Object service, List<Annotation> qualifiers) {
@@ -129,15 +142,15 @@ public class ServicePublisher {
         if (publish.rank() != 0) {
             properties.setProperty("service.rank", publish.rank() + "");
         }
-        if(contracts.length > 0) {// if there are contracts
+        if (contracts.length > 0) {// if there are contracts
             String[] names = new String[contracts.length];
-            for(int i = 0;i < contracts.length;i++) {
+            for (int i = 0; i < contracts.length; i++) {
                 names[i] = contracts[i].getName();
                 logger.info("Registering OSGi service {} as {}", clazz.getName(), names[i]);
             }
             registration = bundle.getBundleContext().registerService(names, service, properties);
         } else {
-            if(clazz.getInterfaces().length > 0) {
+            if (clazz.getInterfaces().length > 0) {
                 List<Class> interfaces = new ArrayList<Class>();
                 for (Class itf : clazz.getInterfaces()) {
                     if (!blackList.contains(itf.getName())) {
@@ -146,16 +159,16 @@ public class ServicePublisher {
                 }
                 contracts = interfaces.toArray(new Class[interfaces.size()]);
             }
-            if(contracts.length > 0) {// if there are non-blacklisted interfaces
+            if (contracts.length > 0) {// if there are non-blacklisted interfaces
                 String[] names = new String[contracts.length];
-                for(int i = 0;i < contracts.length;i++) {
+                for (int i = 0; i < contracts.length; i++) {
                     names[i] = contracts[i].getName();
                     logger.info("Registering OSGi service {} as {}", clazz.getName(), names[i]);
                 }
                 registration = bundle.getBundleContext().registerService(names, service, properties);
             } else {
                 Class superClass = clazz.getClass().getSuperclass();
-                if(superClass != null && superClass != Object.class) {// if there is a superclass
+                if (superClass != null && superClass != Object.class) {// if there is a superclass
                     logger.info("Registering OSGi service {} as {}", clazz.getName(), superClass.getName());
                     registration = bundle.getBundleContext().registerService(superClass.getName(), service, properties);
                 } else {// publish directly with the implementation type
