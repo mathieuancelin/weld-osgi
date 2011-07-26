83a2b76866aa61a4218fdf82ce3c46fc03d1edc1
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-07-26 15:12:56 +0200

    Fix #47 : change file headers

--- cdi-osgi-core/cdi-osgi-extension-impl/src/main/java/org/osgi/cdi/impl/integration/ServicePublisher.java
+++ cdi-osgi-core/cdi-osgi-extension-impl/src/main/java/org/osgi/cdi/impl/integration/ServicePublisher.java
@@ -1,4 +1,9 @@
-/**
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.osgi.cdi.impl.integration;
 
 import org.osgi.cdi.api.extension.annotation.Property;
