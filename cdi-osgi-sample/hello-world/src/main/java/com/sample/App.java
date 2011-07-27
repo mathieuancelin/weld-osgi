20d3e841ce9a7e0d027013e87a7cd16ca474e34d
Matthieu Clochard <clochard.matthieu@gmail.com>
2011-07-27 13:56:49 +0200

    User doc preface and getting started and hello world step 1 example

---
+++ cdi-osgi-sample/hello-world/src/main/java/com/sample/App.java
@@ -0,0 +1,23 @@
+package com.sample;
+
+import com.sample.api.HelloWorld;
+import org.osgi.cdi.api.extension.events.BundleContainerEvents;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+public class App {
+
+    @Inject
+    HelloWorld helloWorld;
+
+    public void onStartup(@Observes BundleContainerEvents.BundleContainerInitialized event) {
+        //say hello when the bundle has initialized
+        helloWorld.sayHello();
+    }
+
+    public void onShutdown(@Observes BundleContainerEvents.BundleContainerShutdown event) {
+        //say good bye when the bundle has shutdown
+        helloWorld.sayGoodbye();
+    }
+}
